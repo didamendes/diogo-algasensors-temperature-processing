ad98a8e91730375a3f5cf3a0a3ee86002d98edee
didamendes <didamendes@hotmail.com>
2025-04-18 18:56:51 -0300

    Criação do endpoints do micro servicos

--- src/main/java/br/com/diogomendes/algasensors/temperature/processing/api/controller/TemperatureProcessingController.java
+++ src/main/java/br/com/diogomendes/algasensors/temperature/processing/api/controller/TemperatureProcessingController.java
@@ -3,20 +3,26 @@ package br.com.diogomendes.algasensors.temperature.processing.api.controller;
 import br.com.diogomendes.algasensors.temperature.processing.api.model.TemperatureLogOutput;
 import br.com.diogomendes.algasensors.temperature.processing.common.IdGenerator;
 import io.hypersistence.tsid.TSID;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
+import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.OffsetDateTime;
 
+import static br.com.diogomendes.algasensors.temperature.processing.infraestructure.rabbitmq.RabbitMQConfig.FANOUT_EXCHANGE_NAME;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
 
 @RestController
 @RequestMapping("/api/sensors/{sensorId}/temperatures/data")
 @Slf4j
+@RequiredArgsConstructor
 public class TemperatureProcessingController {
+
+    private final RabbitTemplate rabbitTemplate;
 
     @PostMapping(consumes = TEXT_PLAIN_VALUE)
     public void data(@PathVariable TSID sensorId, @RequestBody String input) {
@@ -39,7 +45,12 @@ public class TemperatureProcessingController {
                 .registeredAt(OffsetDateTime.now())
                 .build();
 
-        log.info(temperatureLogOutput.toString());
+        MessagePostProcessor messagePostProcessor = message -> {
+            message.getMessageProperties().setHeader("sensorId", temperatureLogOutput.getSensorId().toString());
+            return message;
+        };
+
+        rabbitTemplate.convertAndSend(FANOUT_EXCHANGE_NAME, "", temperatureLogOutput, messagePostProcessor);
     }
 
 }
